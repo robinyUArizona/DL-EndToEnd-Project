10d7dd20b1c07549eb3bc3c12e6c78ba8666e4cd
robinyUArizona <robiny@arizona.edu>
2024-02-23 20:50:30 +0545

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# from src.cnnClassifier import logger
-from cnnClassifier import logger 
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-logger.info("Welcome to cnnClassifier")
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
